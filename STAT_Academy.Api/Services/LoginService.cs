afc534150742d06cb39ca35f8e0faa92436a8dca
Zick <cihc2006@gmail.com>
2026-06-03 17:21:36 -0600

    Actualizacion de login con encriptacion.

--- STAT_Academy.Api/Services/LoginService.cs
+++ STAT_Academy.Api/Services/LoginService.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
 using STAT_Academy.Api.Data;
+using STAT_Academy.Api.DTOs.Login;
 using STAT_Academy.Api.Models;
 
 namespace STAT_Academy.Api.Services
@@ -7,27 +8,55 @@ namespace STAT_Academy.Api.Services
     public class LoginService
     {
         private readonly ApplicationDbContext _context;
+        private readonly PasswordHasher<UsuarioModel> _passwordHasher;
 
         public LoginService(ApplicationDbContext context)
         {
             _context = context;
+            _passwordHasher = new PasswordHasher<UsuarioModel>();
         }
 
-        public UsuarioModel Login(string email, string password)
+        public LoginResponse? Login(string email, string password)
         {
-            var usuario = _context.Usuario.FirstOrDefault(u =>
-                u.email == email &&
-                u.password == password &&
-                u.estado == true);
+            var usuario = _context.Usuario
+                .FirstOrDefault(u => u.email == email);
 
             if (usuario == null)
+            {
                 return null;
+            }
 
-            usuario.ultimo_Login = DateTime.Now;
+            if (!usuario.estado)
+            {
+                return null;
+            }
+
+            var resultado = _passwordHasher.VerifyHashedPassword(
+                usuario,
+                usuario.password,
+                password
+            );
+
+            if (resultado == PasswordVerificationResult.Failed)
+            {
+                usuario.intentos_login += 1;
+                _context.SaveChanges();
+
+                return null;
+            }
+
+            usuario.intentos_login = 0;
+            usuario.ultimo_Login = DateTime.UtcNow;
 
             _context.SaveChanges();
 
-            return usuario;
+            return new LoginResponse
+            {
+                id = usuario.id,
+                email = usuario.email,
+                nombre = usuario.nombre,
+                fk_Tipo_Usuario = usuario.fk_Tipo_Usuario
+            };
         }
     }
 }
